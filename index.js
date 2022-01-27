a250973ab7061370f7412eaac50701d0a0c14fa3
nahuelkbx <nahueltomasmonserrat@gmail.com>
2022-01-27 16:26:06 -0300

    repasoo

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-const express = require('express')
-const app = require('./app');
+const express = require("express");
+const app = require("./src/app");
+const { conn } = require("./src/db");
+const {Digimon, Episode} = require('./src/db')
 
 const server = express();
 
 server.use(express.json());
 
-// /digimon/find
 
-server.use("/",app)
+let digimons = [
+  {
+    name: "NahuelMon",
+    color: "Azul",
+  },
+  {
+    name: "MatiMon",
+    color: "Rojo",
+  },
+  {
+    name: "CavalloMon",
+    color: "Verde",
+  },
+];
+let episodes = [
+  {
+    name: "EP 1",
+    url: "https://www.youtube.com/watch?v=EOuHn6eiyjI&ab_channel=snakewalkerTK421",
+  },
+  {
+    name: "EP 2",
+    url: "https://www.youtube.com/watch?v=EOuHn6eiyjI&ab_channel=snakewalkerTK421",
+  },
+  {
+    name: "EP 3",
+    url: "https://www.youtube.com/watch?v=EOuHn6eiyjI&ab_channel=snakewalkerTK421",
+  },
+];
 
+server.use("/", app);
 
-server.listen(3001,()=>{
-  console.log("Estoy en el puerto 3001")
-})
+conn.sync({ force: true }).then(() => {
+  digimons.forEach(ele => {
+    Digimon.create({
+      name: ele.name,
+      color: ele.color
+    })
+  })
+  episodes.forEach(ele => {
+    Episode.create({
+      name: ele.name,
+      url: ele.url
+    })
+  })
+  server.listen(3001, () => {
+    console.log("Estoy en el puerto 3001");
+  });
+});
+ 
+
